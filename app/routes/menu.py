c3397e627453e3c2f6bf3f7e100fdda39b782a15
JanhviDhage <157267473+JanhviDhage@users.noreply.github.com>
2025-01-03 00:49:49 +0530

    Delete app/routes/menu.py

--- app/routes/menu.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from app.models import MenuItem
-
-menu_blueprint = Blueprint('menu', __name__)
-
-@menu_blueprint.route('/menu')
-def menu():
-    items = MenuItem.query.all()
-    return render_template('menu.html', items=items)
