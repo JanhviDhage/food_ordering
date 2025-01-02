c723983158e6acb50a945064b88bb4b13aee8f25
JanhviDhage <157267473+JanhviDhage@users.noreply.github.com>
2025-01-03 02:05:49 +0530

    Update menu._routes.py

--- menu._routes.py
+++ menu._routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
 from app.models import MenuItem
 
-menu_blueprint = Blueprint('menu', __name__)
+menu_bp = Blueprint('menu', __name__)
 
-@menu_blueprint.route('/menu')
+@menu_bp.route('/menu')
 def menu():
     items = MenuItem.query.all()
     return render_template('menu.html', items=items)
