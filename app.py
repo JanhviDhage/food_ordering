2c398bba011d52855b8aadfa7d024bf62e0e9464
JanhviDhage <157267473+JanhviDhage@users.noreply.github.com>
2025-01-03 02:47:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from routes import menu_bp, order_bp, payment_bp
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Import routes after initializing `db`
-from routes import *
+# Register Blueprints
+app.register_blueprint(menu_bp, url_prefix='/menu')
+app.register_blueprint(order_bp, url_prefix='/order')
+app.register_blueprint(payment_bp, url_prefix='/payment')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
